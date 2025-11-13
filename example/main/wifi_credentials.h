43a95883f94747bebce515a16a5bbe9f4ee74302
mccannex <robot@mccannex.net>
2025-11-12 22:38:44 -0700

    add stub wifi credentials

---
+++ example/main/wifi_credentials.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+ * @file wifi_credentials.h
+ * @brief WiFi credentials configuration template
+ * 
+ * Stub file for keeping my credentials out of my repository
+ */
+
+#define WIFI_SSID     "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
